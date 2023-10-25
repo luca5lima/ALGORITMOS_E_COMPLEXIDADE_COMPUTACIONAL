8aaba482e490ae286c3f7a6f91cfe5f4d98cd81c
Lucas Lima Cavalcante <lucaslimoa2000@gmail.com>
2023-10-25 16:43:16 -0300

    Implementando funções

--- Vermelho e Preto/modulo.cpp
+++ Vermelho e Preto/modulo.cpp
@@ -56,18 +56,71 @@ Tno * rotacao_simples_direita(Tno *a){//mesmo código da AVL
 }
 
 Tno* criar(int elemento){//mesmo código da AVL
-
+  Tno* dado = (Tno*) malloc(sizeof(Tno));
+  dado->elemento = elemento;
+  dado->esq = NULL;
+  dado->dir = NULL;
+  dado->cor = vermelho;
+  return dado;
 }
 Tno* inserir(Tno* a, int valor){
-
+  Tno* dado = criar(valor);
+  a = incluir_no(a, dado);
+  if (a != NULL)
+    a->cor = preto;
+  return a;
 }
 Tno* incluir_no(Tno* a, Tno* dado){
-
+  if(a == NULL)
+    return dado;
+  else
+  {
+    if (a->elemento > a->elemento)
+      a->esq = incluir_no(a->esq, dado);
+    else
+      a->dir = incluir_no(a->dir, dado);
+    //Analisar a cor
+    if ((cor(a->esq) == vermelho) && (cor(a->dir) == vermelho))
+      trocaCor(a);
+    //Analisar as rotações
+    if( (cor(a->dir) == vermelho) && (cor(a->dir->dir) == vermelho)){  //Mesmo lado dir->dir
+      a->cor = vermelho;
+      a->dir->cor = preto;
+      a = rotacao_simples_esquerda(a);
+    }
+    if( (cor(a->dir) == vermelho) && (cor(a->dir->esq) == vermelho)){  //Lados opostos
+      //Rotação Dupla
+      a->dir = rotacao_simples_direita(a->dir);
+      a->cor = vermelho;
+      a->dir->cor = preto;
+      a = rotacao_simples_esquerda(a);
+    }
+    if( (cor(a->esq) == vermelho) && (cor(a->esq->esq) == vermelho)){  //Mesmo lado esq->esq
+      a->cor = vermelho;
+      a->esq->cor = preto;
+      a = rotacao_simples_direita(a);
+    }
+    if( (cor(a->esq) == vermelho) && (cor(a->esq->dir) == vermelho)){  //Lados opostos 
+      //Rotação Dupla
+      a->esq = rotacao_simples_esquerda(a->esq);
+      a->cor = vermelho;
+      a->esq->cor = preto;
+      a = rotacao_simples_direita(a);
+    }
+  }
 }
 int cor(Tno* a){
-
+  if (a == NULL)
+    return preto;
+  else
+    return a->cor;  
 }
 void trocaCor(Tno* a){
+  a->cor = !a->cor;
+  if(a->cor != NULL)
+    a->esq->cor = !a->esq->cor;
+  if(a->cor != NULL)
+    a->dir->cor = !a->dir->cor;
 
 }
 void imprime(Tno* a){//mesmo código da AVL
